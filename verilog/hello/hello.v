4dc01157871dca2f0b030909d4b3ded44f41d20e
Tsuyoshi Kadookura <39421054+kadokuratsuyoshi@users.noreply.github.com>
2018-11-17 06:39:15 +0900

    Create hello.v

---
+++ verilog/hello/hello.v
@@ -0,0 +1,23 @@
+module hello (
+        rst,
+        clk,
+        d,
+        q
+    );
+    input		rst;
+    input		clk;
+    input       d;
+    output      q;
+
+    reg     r;
+    assign q = r;
+    always @ (posedge clk or negedge rst ) begin
+        if ( !rst ) begin
+            r <= 1'b0;
+        end
+        else begin
+            r <= ~d;
+        end
+    end
+endmodule
+//  eof
