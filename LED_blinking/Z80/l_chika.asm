c3dd7ff73c182ef3fcc12cdd9a52d2a7e60764ae
Tsuyoshi Kadookura <39421054+kadokuratsuyoshi@users.noreply.github.com>
2018-11-23 08:14:12 +0900

    Create l_chika.asm

---
+++ LED_blinking/Z80/l_chika.asm
@@ -0,0 +1,18 @@
+PIOAD:  equ     0
+PIOAC:  equ     1
+
+        org     0
+        ld      sp,0
+;       8255A PIO setting
+        ld      a,11001111b ;bit mode setting
+        out     PIOAC,a
+        ld      a,00001111b ;0:output, 1:input
+        out     PIOAC,a
+;       MAIN
+        ld      a,0
+        ld      b,0
+BLINK:  inc     a
+        out     PIOAD,a
+DELAY:  inc     b
+        jp      nz,DELAY
+        jp      BLINK
