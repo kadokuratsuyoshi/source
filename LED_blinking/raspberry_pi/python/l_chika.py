3f1c65e7b6197244e09f2c4448cf6a718319ab76
Tsuyoshi Kadookura <39421054+kadokuratsuyoshi@users.noreply.github.com>
2018-11-23 08:15:40 +0900

    Create l_chika.py

---
+++ LED_blinking/raspberry_pi/python/l_chika.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(3, GPIO.OUT)
+
+while True:
+    GPIO.output(3, GPIO.HIGH)
+    sleep(0.5)
+    GPIO.output(3, GPIO.LOW)
+    sleep(0.5)
