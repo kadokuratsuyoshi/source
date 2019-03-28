caae97b1e43ea97ae28558d6a003db6ef70f4ae4
Tsuyoshi Kadookura <39421054+kadokuratsuyoshi@users.noreply.github.com>
2019-03-28 18:54:52 +0900

    Create fft.h

---
+++ OpenMP/qv/fft.h
@@ -0,0 +1,3 @@
+extern void fft(complex <double> x[], int n, int flag);
+extern void fft2D(complex <double> x[], int n1, int n2, int flag);
+extern void fft3D(complex <double> x[], int n1, int n2, int n3, int flag);
