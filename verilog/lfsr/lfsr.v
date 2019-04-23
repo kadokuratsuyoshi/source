728c25db333c491109cd460b31e2da4b4d46ba5a
Tsuyoshi Kadookura <39421054+kadokuratsuyoshi@users.noreply.github.com>
2019-04-23 16:48:18 +0900

    Create lfsr.v

---
+++ verilog/lfsr/lfsr.v
@@ -0,0 +1,16 @@
+module lfsr (out, clk, rst);
+
+	output	reg [3:0] out;
+	input	clk, rst;
+	wire	fb;
+	assign	fb = ~(out[3] ^ out[2]);
+
+	always @(posedge clk or posedge rst) begin
+		if (rst) begin
+			out <= 4'b0;
+		end
+		else begin
+			out <= {out[2:0],fb};
+		end
+	end
+endmodule
