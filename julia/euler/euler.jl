14e4cadf2dcc9a2c8ff83147fa1088f437cd6366
Tsuyoshi Kadookura <39421054+kadokuratsuyoshi@users.noreply.github.com>
2019-08-06 08:59:10 +0900

    Create euler.jl

---
+++ julia/euler/euler.jl
@@ -0,0 +1,21 @@
+#
+#	program:	euler.jl
+#	contents:	numerical solution of m(d^2/dt^2)x=-kx-av by Euler method
+#
+function euler()
+	t = 0.0
+	dt = 0.01
+	x = 1.0
+	v = 0.0
+	k = 1.0
+	m = 1.0
+	a = 1.0
+	for t in (0:dt:10)
+		println("$t $x $v")
+		dx = v * dt
+		dv = (-1.0*(k/m)*x - (a/m)*v) * dt
+		x = x + dx
+		v = v + dv
+	end
+end
+euler()
