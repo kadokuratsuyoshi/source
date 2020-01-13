69a981268d16c4ca664fd0c07dcb696ba2eaad73
Tsuyoshi Kadookura <39421054+kadokuratsuyoshi@users.noreply.github.com>
2020-01-14 05:33:37 +0900

    Update lfsr.v

--- verilog/lfsr/lfsr.v
+++ verilog/lfsr/lfsr.v
@@ -1,17 +1,17 @@
 //	4bit linear feedback shift register
 module lfsr (out, clk, rst);
 
-	output	reg [3:0] out;
-	input	clk, rst;
-	wire	fb;
-	assign	fb = ~(out[3] ^ out[2]);
+    output	reg [3:0] out;
+    input	clk, rst;
+    wire	fb;
+    assign	fb = ~(out[3] ^ out[2]);
 
-	always @(posedge clk) begin
-		if (!rst) begin
-			out <= 4'b0;
-		end
-		else begin
-			out <= {out[2:0],fb};
-		end
-	end
+    always @(posedge clk) begin
+        if (!rst) begin
+            out <= 4'b0;
+        end
+        else begin
+            out <= {out[2:0],fb};
+        end
+    end
 endmodule
