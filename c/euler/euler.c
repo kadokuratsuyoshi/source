47b3a1dfefad54811ae36eaca3819f65ef37dbfb
Tsuyoshi Kadookura <39421054+kadokuratsuyoshi@users.noreply.github.com>
2020-08-23 15:04:08 +0900

    Update euler.c

--- c/euler/euler.c
+++ c/euler/euler.c
@@ -10,17 +10,18 @@ int main(void){
 	double dt = 0.01;
 	double x = 1.0;
 	double v = 0.0;
-    double dx, dv;
-    double k = 1.0;
-    double m = 1.0;
-    double a = 1.0;
+	double dx, dv;
+	double k = 1.0;
+	double m = 1.0;
+	double gamma = 1.0;
 
 	for (t=0; t<10; t=t+dt ) {
-        dx = v * dt;
-        dv = (-1.0*(k/m)*x - (a/m)*v) * dt;
-        x = x + dx;
-        v = v + dv;
-		printf("%f %f %f\n", t, x, v );
+		printf("%f %f %f\n", t, x, v);
+		dx = v * dt;
+		dv =  ((-k*x - gamma*v)/m) * dt;
+        	x = x + dx;
+        	v = v + dv;
+		t = t + dt;
 	}
 	return 0;
 }
