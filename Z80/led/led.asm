2d34a5c05863438027b02af81b41c156c90d3c9a
Tsuyoshi Kadookura <39421054+kadokuratsuyoshi@users.noreply.github.com>
2020-09-19 04:42:47 +0900

    Update led.asm

--- Z80/led/led.asm
+++ Z80/led/led.asm
@@ -2,17 +2,17 @@ PIOAD:  equ     0
 PIOAC:  equ     1
 
         org     0
+RESET:
         ld      sp,0
 ;       8255A PIO setting
         ld      a,11001111b ;bit mode setting
         out     PIOAC,a
         ld      a,00001111b ;0:output, 1:input
         out     PIOAC,a
-;       MAIN
         ld      a,0
         ld      b,0
-BLINK:  inc     a
+MAIN:  inc     a
         out     PIOAD,a
 DELAY:  inc     b
         jp      nz,DELAY
-        jp      BLINK
+        jp      MAIN
