82c1fdb22ea6eba08e10603e431241a1a2c96d9a
Tsuyoshi Kadookura <39421054+kadokuratsuyoshi@users.noreply.github.com>
2020-11-11 07:46:45 +0900

    Update rk.py

--- python/runge-kutta/rk.py
+++ python/runge-kutta/rk.py
@@ -1,36 +1,37 @@
-def rk_v( t, x, v ):
+#!/usr/bin/env python3
+#-*- coding:utf-8 -*-
+def rk_v(t, x, v):
     return v
 
-def rk_a( t, x, v):
-    k = 1.0
-    m = 1.0
-    a = 1.0
-    return ( -1.0*(k/m)*x - (a/m)*v )
+def rk_a(t, x, v):
+    k=1.0
+    m=1.0
+    gamma=1.0
+    return ((-k*x-gamma*v)/m)
 
-t = 0
-tmax = 10.0
-dt = 0.01
-x = 1.0
-v = 0.0
-n = 1000
-dx = 0.0
-dy = 0.0
-k1 = [0, 1]
-k2 = [0, 1]
-k3 = [0, 1]
-k4 = [0, 1]
-for t in range(n):
-    print("%f %f %f" % (t, x, v))
-    k1[0] = rk_v( t, x, v ) * dt
-    k1[1] = rk_a( t, x, v ) * dt
-    k2[0] = rk_v( t+dt/2.0, x+k1[0]/2.0, v+k1[1]/2.0) * dt
-    k2[1] = rk_a( t+dt/2.0, x+k1[0]/2.0, v+k1[1]/2.0) * dt
-    k3[0] = rk_v( t+dt/2.0, x+k2[0]/2.0, v+k2[1]/2.0) * dt
-    k3[1] = rk_a( t+dt/2.0, x+k2[0]/2.0, v+k2[1]/2.0) * dt
-    k4[0] = rk_v( t+dt, x+k3[0], v+k3[1] ) * dt
-    k4[1] = rk_a( t+dt, x+k3[0], v+k3[1] ) * dt
-    dx = (k1[0] + 2.0*k2[0] + 2.0*k3[0] + k4[0]) / 6.0
-    dv = (k1[1] + 2.0*k2[1] + 2.0*k3[1] + k4[1]) / 6.0
-    x = x + dx
-    v = v + dv
-    t = t + dt
+def main():
+    t=0.0
+    x=1.0
+    v=0.0
+    dt=0.01
+    rdx=[0,1,2,3]
+    rdv=[0,1,2,3]
+    n=1000
+    for i in range(n):
+        print("%f %f %f" % (t, x, v))
+        rdx[0] = dt * rk_v(t, x, v )
+        rdx[1] = dt * rk_v(t+dt/2, x+rdx[0]/2, v+rdv[0]/2)
+        rdx[2] = dt * rk_v(t+dt/2, x+rdx[1]/2, v+rdv[1]/2)
+        rdx[3] = dt * rk_v(t+dt,   x+rdx[2],   v+rdv[2])
+        rdv[0] = dt * rk_a(t, x, v)
+        rdv[1] = dt * rk_a(t+dt/2, x+rdx[0]/2, v+rdv[0]/2)
+        rdv[2] = dt * rk_a(t+dt/2, x+rdx[1]/2, v+rdv[1]/2)
+        rdv[3] = dt * rk_a(t+dt,   x+rdx[2],   v+rdv[2])
+        dx = (rdx[0]+rdx[1]*2+rdx[2]*2+rdx[3])/6
+        dv = (rdv[0]+rdv[1]*2+rdv[2]*2+rdv[3])/6
+        t = t + dt
+        x = x + dx
+        v = v + dv
+
+if __name__ == '__main__':
+    main()
