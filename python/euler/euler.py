05781bb6a829419b5996fb690db8d67c56fbdef8
Tsuyoshi Kadookura <39421054+kadokuratsuyoshi@users.noreply.github.com>
2020-11-11 07:35:06 +0900

    Update euler.py

--- python/euler/euler.py
+++ python/euler/euler.py
@@ -1,17 +1,21 @@
-t = 0
-n = 1000
-dt = 0.01
-x = 1.0
-v = 0.0
-dx = 0.0
-dv = 0.0
-k = 1.0
-m = 1.0
-gamma = 1.0
-for t in range(n):
-    print("%f %f %f" % (t, x, v))
-    dx = v * dt
-    dv = ( (-k*x)/m - (gamma*v)/m ) * dt
-    x = x + dx
-    v = v + dv
-    t = t * dt
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+def main():
+    t = 0
+    x = 1.0
+    v = 0.0
+    dt = 0.01
+    k = 1.0
+    m = 1.0
+    gamma = 1.0
+    n = 1000
+    for i in range(n):
+        print("%f %f %f" % (t, x, v))
+        dx = dt * v
+        dv = dt * ((-k*-gamma*v)/m)
+        t = t + dt
+        x = x + dx
+        v = v + dv
+        
+if __name__ == '__main__':
+    main()
