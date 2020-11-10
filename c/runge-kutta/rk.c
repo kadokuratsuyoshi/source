80509663654eac56357a51a51ebc2de54f17b5ad
Tsuyoshi Kadookura <39421054+kadokuratsuyoshi@users.noreply.github.com>
2020-11-11 07:16:12 +0900

    Update rk.c

--- c/runge-kutta/rk.c
+++ c/runge-kutta/rk.c
@@ -31,7 +31,7 @@ int main(void)
         rdv[0] = dt * rk_a(t, x, v);
         rdv[1] = dt * rk_a(t+dt/2, x+rdx[0]/2, v+rdv[0]/2);
         rdv[2] = dt * rk_a(t+dt/2, x+rdx[1]/2, v+rdv[1]/2);
-        rdv[3] = dt * rk_a(t+dt,   x+rdx[2],   v+rdv[3]);
+        rdv[3] = dt * rk_a(t+dt,   x+rdx[2],   v+rdv[2]);
         dx = (rdx[0]+2*rdx[1]+2*rdx[2]+rdx[3]) / 6;
         dv = (rdv[0]+2*rdv[1]+2*rdv[2]+rdv[3]) / 6;
 	t = t + dt;
