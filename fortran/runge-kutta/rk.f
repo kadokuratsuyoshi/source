220a8cdfcba2efcea96fb3b81912e7a61c34ab56
Tsuyoshi Kadookura <39421054+kadokuratsuyoshi@users.noreply.github.com>
2020-11-21 18:40:26 +0900

    Update rk.f

--- fortran/runge-kutta/rk.f
+++ fortran/runge-kutta/rk.f
@@ -1,23 +1,23 @@
       program rk
-      real k1(0:1), k2(0:1), k3(0:1), k4(0:1)
+      real rdx(0:3), kdv(0:3)
       t = 0
-      dt = 0.01
       x = 1
       v = 0
+      dt = 0.01
 100   write(6,*) t, x, v
-      k1(0) = rk_v(t, x, v) * dt
-      k1(1) = rk_a(t, x, v) * dt
-      k2(0) = rk_v(t+dt/2.0,x+k1(0)/2.0,v+k1(1)/2.0) * dt
-      k2(1) = rk_a(t+dt/2.0,x+k1(0)/2.0,v+k1(1)/2.0) * dt
-      k3(0) = rk_v(t+dt/2.0,x+k2(0)/2.0,v+k2(1)/2.0) * dt
-      k3(1) = rk_a(t+dt/2.0,x+k2(0)/2.0,v+k2(1)/2.0) * dt
-      k4(0) = rk_v(t+dt,x+k3(0),v+k3(1)) * dt
-      k4(1) = rk_a(t+dt,x+k3(0),v+k3(1)) * dt
-      dx = (k1(0)+2.0*k2(0)+2.0*k3(0)+k4(0)) / 6.0
-      dv = (k1(1)+2.0*k2(1)+2.0*k3(1)+k4(1)) / 6.0
+      rdx(0) = dt * rk_v(t, x, v)
+      rdx(1) = rk_v(t+dt/2,x+k1(0)/2,v+k1(1)/2) * dt
+      rdx(2) = rk_v(t+dt/2,x+k2(0)/2,v+k2(1)/2) * dt
+      rdx(3) = rk_v(t+dt,x+k3(0),v+k3(1)) * dt
+      rdv(0) = dt * rk_a(t, x, v)
+      rdv(1) = rk_a(t+dt/2,x+k1(0)/2,v+k1(1)/2) * dt
+      rdv(2) = rk_a(t+dt/2,x+k2(0)/2,v+k2(1)/2) * dt
+      rdv(3) = rk_a(t+dt,x+k3(0),v+k3(1)) * dt
+      dx = (rdx(0)+2*(rdx(1)+rdx(2))+rdx(3)) / 6
+      dv = (rdv(0)+2*(rdv(1)+rdv(2))+rdv(3)) / 6
+      t = t + dt
       x = x + dx
       v = v + dv
-      t = t + dt
       if ( t .le. 10 ) goto 100
       end program rk
 
@@ -29,7 +29,7 @@
       function rk_a(t, x, v)
       k = 1.0
       m = 1.0
-      a = 1.0
-      rk_a = -1.0*(k/m)*x - (a/m)*v
+      gamma = 1.0
+      rk_a = ((-k*x-gamma*v)/m)
       return
       end function rk_a
