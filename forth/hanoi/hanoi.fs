d10802e614093f179175215bcf84f4fa37ce44a1
Tsuyoshi Kadookura <39421054+kadokuratsuyoshi@users.noreply.github.com>
2020-11-23 19:56:41 +0900

    Create hanoi.fs

---
+++ forth/hanoi/hanoi.fs
@@ -0,0 +1,20 @@
+( gforth, >include hanoi.fs, >1 2 3 3 hanoi )
+variable cnt
+
+: hanoi-print
+    cr 2dup . ." -> " . ;
+
+: hanoi-impl
+    dup >r 0> if
+        rot swap r@ 1- recurse
+        cnt @ 1+ cnt ! 
+		hanoi-print
+        rot r@ 1- recurse
+        swap
+    then
+    r> drop ;
+
+: hanoi
+    0 cnt !
+    hanoi-impl drop drop drop
+    cr ." count: " cnt @ . cr ;
