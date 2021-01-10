d9c4f19585eefba2f07290730dfd519083e7333a
Tsuyoshi Kadookura <39421054+kadokuratsuyoshi@users.noreply.github.com>
2021-01-10 10:10:06 +0900

    Add files via upload

---
+++ V850/hello/sci.h
@@ -0,0 +1,12 @@
+/*
+	Filename: TinyBASIC/sci.h
+	Language: CA850(PM+)
+*/
+
+void vInit_uart(void);
+void c_putch(unsigned char);
+unsigned char c_kbhit(void);
+unsigned char c_getch(void);
+//void putch2(char);
+//unsigned char kbhit2(void);
+//char getch2(void);
