35ef3d42d80724e5e9853e590eef472d01565a2a
Tsuyoshi Kadookura <39421054+kadokuratsuyoshi@users.noreply.github.com>
2021-03-19 10:49:09 +0900

    Add files via upload

--- perl/euler/euler.pl
+++ perl/euler/euler.pl
@@ -8,11 +8,11 @@ my $v = 0.0;
 my $dt = 0.01;
 my $k = 1.0;
 my $m = 1.0;
-my $gamma = 1.0;
+my $a = 1.0;
 for (my $n=0; $n<1000; $n++) {
 	print "$t $x $v\n";
 	my $dx = $dt * $v;
-	my $dv = $dt * ((-$k * $x - $gamma * $v)/$m);
+	my $dv = $dt * ((-$k * $x - $a * $v)/$m);
 	$t = $t + $dt;
 	$x = $x + $dx;
 	$v = $v + $dv;
