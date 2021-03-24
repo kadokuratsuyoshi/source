ae5307f3f04943096386c18067094aa894d1525b
Tsuyoshi Kadookura <39421054+kadokuratsuyoshi@users.noreply.github.com>
2021-03-24 09:24:40 +0900

    Add files via upload

---
+++ tcl/runge-kutta/go.bat
@@ -0,0 +1,4 @@
+@echo off
+rem #!/bin/bash
+tclsh rk.tcl > out0
+gnuplot rk.gpi
--- tcl/runge-kutta/rk.tcl
+++ tcl/runge-kutta/rk.tcl
@@ -1,3 +1,4 @@
+#
 proc rk_v { t x v } {
     return  $v 
 }
@@ -5,32 +6,30 @@ proc rk_v { t x v } {
 proc rk_a { t x v } {
     set k 1.0
     set m 1.0
-    set a 1.0
-    return [ expr -1.0 * ($k/$m)*$x - ($a/$m)*$v ]
+    set gamma 1.0
+    return [expr ((-$k*$x-$gamma*$v)/$m) ]
 }
 
 proc rk {} {
-    set N 10
     set t 0.0
-    set dt 0.01
     set x 1.0
     set v 0.0
-    set dx 0.0
-    set dv 0.0
-    for {set t 0} {[expr $t <= $N]} { set t [expr $t + $dt] } {
-        set k1(1) [expr $dt * [rk_v $t $x $v] ]
-        set k1(2) [expr $dt * [rk_a $t $x $v] ]
-        set k2(1) [expr $dt * [rk_v $t+$dt/2.0 $x+$k1(1)/2.0 $v+$k1(2)/2.0] ]
-        set k2(2) [expr $dt * [rk_a $t+$dt/2.0 $x+$k1(1)/2.0 $v+$k1(2)/2.0] ]
-        set k3(1) [expr $dt * [rk_v $t+$dt/2.0 $x+$k2(1)/2.0 $v+$k2(2)/2.0] ]
-        set k3(2) [expr $dt * [rk_a $t+$dt/2.0 $x+$k2(1)/2.0 $v+$k2(2)/2.0] ]
-        set k4(1) [expr $dt * [rk_v $t+$dt $x+$k3(1) $v+$k3(2)] ]
-        set k4(2) [expr $dt * [rk_a $t+$dt $x+$k3(1) $v+$k3(2)] ]
-        set dx [expr ($k1(1)+2.0*$k2(1)+2.0*$k3(1)+$k4(1))/6.0 ]
-        set dv [expr ($k1(2)+2.0*$k2(2)+2.0*$k3(2)+$k4(2))/6.0 ]
+    set dt 0.01
+    for {set n 0} {[expr $n <= 1000]} { set n [expr $n + 1] } {
+        puts "$t $x $v"
+        set kx(1) [expr $dt * [rk_v $t          $x            $v          ] ]
+        set kv(1) [expr $dt * [rk_a $t          $x            $v          ] ]
+        set kx(2) [expr $dt * [rk_v ($t+$dt/2) ($x+$kx(1)/2) ($v+$kv(1)/2)] ]
+        set kv(2) [expr $dt * [rk_a ($t+$dt/2) ($x+$kx(1)/2) ($v+$kv(1)/2)] ]
+        set kx(3) [expr $dt * [rk_v ($t+$dt/2) ($x+$kx(2)/2) ($v+$kv(2)/2)] ]
+        set kv(3) [expr $dt * [rk_a ($t+$dt/2) ($x+$kx(2)/2) ($v+$kv(2)/2)] ]
+        set kx(4) [expr $dt * [rk_v ($t+$dt)   ($x+$kx(3))   ($v+$kv(3))  ] ]
+        set kv(4) [expr $dt * [rk_a ($t+$dt)   ($x+$kx(3))   ($v+$kv(3))  ] ]
+        set dx [expr ($kx(1)+2*($kx(2)+$kx(3))+$kx(4))/6 ]
+        set dv [expr ($kv(1)+2*($kv(2)+$kv(3))+$kv(4))/6 ]
+		set t [expr $t+$dt]
         set x [expr $x+$dx]
         set v [expr $v+$dv]
-        puts "$t,$x,$v"
     }
 }
 rk
