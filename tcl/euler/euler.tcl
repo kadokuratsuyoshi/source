fb9287e6cf5d878328f03cf920f054ee643e883c
Tsuyoshi Kadookura <39421054+kadokuratsuyoshi@users.noreply.github.com>
2021-03-24 09:23:34 +0900

    Add files via upload

--- tcl/euler/euler.tcl
+++ tcl/euler/euler.tcl
@@ -1,20 +1,18 @@
 proc euler {} {
-    set N 10
-    set t 0.0
-    set dt 0.01
-    set x 1.0
-    set v 0.0
-    set dx  0
-    set dv  0
-    set k 1
-    set m 1
-    set a 1
-    for {set t 0} {[expr $t <= $N]} { set t [expr $t + $dt] } {
-        set dx [expr $v * $dt]
-        set dv [expr (-1.0 * ($k/$m) * $x - ($a/$m) * $v) * $dt]
-        set x  [expr $x + $dx]
-        set v  [expr $v + $dv]
-        puts "$t,$x,$v"
-    }
+	set t 0.0
+	set x 1.0
+	set v 0.0
+	set dt 0.01
+	set k 1.0
+	set m 1.0
+	set gamma 1.0
+	for {set n 0} {[expr $n <= 1000]} { set n [expr $n + 1] } {
+		puts "$t $x $v"
+		set dx [expr $dt * $v]
+		set dv [expr $dt * ((-$k*$x-$gamma*$v)/$m)]
+		set t  [expr $t + $dt]
+		set x  [expr $x + $dx]
+		set v  [expr $v + $dv]
+	}
 }
 euler
