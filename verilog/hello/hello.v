ccc3eb0e620c12fd6cdd3f5429152a4b3bc6479a
Tsuyoshi Kadookura <39421054+kadokuratsuyoshi@users.noreply.github.com>
2021-04-06 06:39:24 +0900

    Update hello.v

--- verilog/hello/hello.v
+++ verilog/hello/hello.v
@@ -1,23 +1,5 @@
-module hello (
-        rst,
-        clk,
-        d,
-        q
-    );
-    input	rst;
-    input	clk;
-    input       d;
-    output      q;
-    reg         r;
-        
-    assign q = r;
-    always @ (posedge clk or negedge rst ) begin
-        if ( !rst ) begin
-            r <= 1'b0;
-        end
-        else begin
-            r <= ~d;
-        end
+module hello;
+    initial begin
+        $display("hello, world");
     end
 endmodule
-//  eof
